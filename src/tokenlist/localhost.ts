15bf5bbc98ce38a5b62afe4187362b723247064a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-01-31 14:16:23 +0000

    checking in testnet addresses & tokenlist

--- src/tokenlist/localhost.ts
+++ src/tokenlist/localhost.ts
@@ -2,7 +2,11 @@ import { getTokenList } from "src/tokenlist/getTokenList";
 import testnetAddressesJson from "src/addresses/testnet.addresses.json";
 
 // Generate the testnet.tokenlist.json file
-getTokenList(testnetAddressesJson, "Testnet token list", "src/tokenlist/testnet.tokenlist.json")
+getTokenList(
+  testnetAddressesJson,
+  "Testnet token list",
+  "src/tokenlist/testnet.tokenlist.json"
+)
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
