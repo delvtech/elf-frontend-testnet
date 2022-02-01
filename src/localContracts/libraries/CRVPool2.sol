9f1fc59ebe6d2702430edffa6956d9c217de4d0b
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-02-01 15:13:08 +0000

    removed localContracts & localTypes as unused

--- src/localContracts/libraries/CRVPool2.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-interface CurveContract {
-    function calc_withdraw_one_coin(uint256 token_amount, uint256 i)
-        external
-        view
-        returns (uint256);
-}
